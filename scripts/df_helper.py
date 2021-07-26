9943e1735cc4c62748cad59e6fab03a281b2ea98
Elias <eandualem@gmail.com>
2021-07-26 10:46:43 +0300

    feat: add DfHelper class

---
+++ scripts/df_helper.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import pandas as pd
+
+sys.path.append(os.path.abspath(os.path.join('../')))
+from log import get_logger
+
+my_logger = get_logger("DfHelper")
+
+
+class DfHelper():
+
+  def __init__(self):
+    pass
+
+  def save_csv(self, df, csv_path, index=False):
+    try:
+      df.to_csv(csv_path, index=index)
+      my_logger.info("file saved as csv")
+
+    except Exception:
+      my_logger.exception("save failed")
+
+  def read_csv(self, csv_path, missing_values=[]):
+    try:
+      df = pd.read_csv(csv_path, na_values=missing_values)
+      my_logger.debug("file read as csv")
+      return df
+    except FileNotFoundError:
+      my_logger.exception("file not found")
