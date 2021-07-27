22c0386a7fd08c9ed1de5af36216a9ee618c20a2
Elias <eandualem@gmail.com>
2021-07-28 00:42:44 +0300

    feat: update notebook

--- scripts/vis_plotly.py
+++ scripts/vis_plotly.py
@@ -11,6 +11,18 @@ class VisPlotly():
   def __init__(self):
     pass
 
+  def mult_hist(self, sr, rows, cols, title_text, subplot_titles, interactive=False):
+    fig = make_subplots(rows=rows, cols=cols, subplot_titles=subplot_titles)
+    for i in range(rows):
+        for j in range(cols):
+            x = ["-> " + str(i) for i in sr[i + j].index]
+            fig.add_trace(go.Bar(x=x, y=sr[i + j].values), row=i + 1, col=j + 1)
+    fig.update_layout(showlegend=False, title_text=title_text)
+    if(interactive):
+        fig.show()
+    else:
+        return Image(pio.to_image(fig, format='png', width=1200))
+
   
 
 
