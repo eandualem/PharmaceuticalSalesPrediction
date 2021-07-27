a9ff1221ff54a603f3f482a23d49b5a784d436b8
Elias <eandualem@gmail.com>
2021-07-27 06:03:18 +0300

    feat: update seaborn plotting helper for mult plot

--- scripts/vis_seaborn.py
+++ scripts/vis_seaborn.py
@@ -3,61 +3,72 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def plot_hist(df: pd.DataFrame, column: str, color: str) -> None:
-  # plt.figure(figsize=(15, 10))
-  # fig, ax = plt.subplots(1, figsize=(12, 7))
-  sns.displot(data=df, x=column, color=color, kde=True, height=7, aspect=2)
-  plt.title(f'Distribution of {column}', size=20, fontweight='bold')
-  plt.show()
+class VisSeaborn():
 
+  def __init__(self):
+    pass
 
-def plot_count(df: pd.DataFrame, column: str) -> None:
-  plt.figure(figsize=(12, 7))
-  sns.countplot(data=df, x=column)
-  plt.title(f'Distribution of {column}', size=20, fontweight='bold')
-  plt.show()
+  def single_plot(self, plot, title, x_labels, y_labels, figsize):
+      fig, ax = plt.subplots(figsize=figsize)
+      _ax = lambda __, _: ax
+      plot(0, 0, _ax)
+      plt.title(title, fontsize=15, fontweight='bold')
+      plt.xlabel(x_labels[0], fontsize=14)
+      plt.ylabel(y_labels[0], fontsize=14)
+      fig.show()
 
+  def vertical_plot(self, plot, rows, title, subtitles, x_labels, y_labels, figsize):
+    fig, ax = plt.subplots(rows, 1, sharex=True, figsize=figsize)
+    _ax = lambda x, _: ax[x]
+    for r in range(rows):
+      ax[r].set_title(subtitles[r])
+      ax[r].set(xlabel=x_labels[r], ylabel='')
+      plot(r, 0, _ax)
+    fig.suptitle(title, fontsize=15, fontweight='bold')
+    fig.subplots_adjust(hspace=0.2)
+    fig.show()
+  
+  def horizontal_plot(self, plot, cols, title, subtitles, x_labels, y_labels, figsize):
+    fig, ax = plt.subplots(1, cols, sharex=True, figsize=figsize)
+    _ax = lambda _, y: ax[y]
+    for c in range(cols):
+      ax[c].set_title(subtitles[c])
+      ax[c].set(xlabel='', ylabel=y_labels[c])
+      plot(0, c, _ax)
+    fig.suptitle(title, fontsize=15, fontweight='bold')
+    fig.subplots_adjust(wspace=0.8)
+    fig.show()
 
-def plot_bar(df: pd.DataFrame, x_col: str, y_col: str, title: str, xlabel: str, ylabel: str) -> None:
-  plt.figure(figsize=(12, 7))
-  sns.barplot(data=df, x=x_col, y=y_col)
-  plt.title(title, size=20)
-  plt.xticks(rotation=75, fontsize=14)
-  plt.yticks(fontsize=14)
-  plt.xlabel(xlabel, fontsize=16)
-  plt.ylabel(ylabel, fontsize=16)
-  plt.show()
+  def square_plots(self, plot, rows, cols, title, subtitles, x_labels, y_labels, figsize):
+    fig, ax = plt.subplots(rows, cols, sharex=True, figsize=figsize)
+    _ax = lambda x, y: ax[x, y]
+    for r in range(rows):
+      for c in range(cols):
+        ax[r, c].set_title(subtitles[(r * cols) + c])
+        ax[r, c].set(xlabel=x_labels[(r * cols) + c], ylabel=y_labels[(r * cols) + c])
+        plot(r, c, _ax)
+    fig.suptitle(title, fontsize=15, fontweight='bold')
+    fig.subplots_adjust(hspace=0.2, wspace=0.2)
+    fig.show()
 
+  def subplots(self, plot, rows, cols, title, subtitles, x_labels, y_labels, figsize):
+    if(subtitles == ""):
+      subtitles = [""]*rows*cols
+    if(x_labels == ""):
+      x_labels = ["x"]*rows*cols
+    if(y_labels == ""):
+      y_labels = ["y"]*rows*cols
 
-def plot_heatmap(df: pd.DataFrame, title: str, cbar=False) -> None:
-  plt.figure(figsize=(12, 7))
-  sns.heatmap(df, annot=True, cmap='viridis', vmin=0,
-              vmax=1, fmt='.2f', linewidths=.7, cbar=cbar)
-  plt.title(title, size=18, fontweight='bold')
-  plt.show()
+    if(rows == 1 and cols==1):
+      return self.single_plot(plot, title, x_labels, y_labels, figsize)
+    if(rows == 1):
+      return self.horizontal_plot(plot, cols, title, subtitles, x_labels, y_labels, figsize)
+    elif(cols == 1):
+      return self.vertical_plot(plot, rows, title, subtitles, x_labels, y_labels, figsize)
+    else:
+      return self.square_plots(plot, rows, cols, title, subtitles, x_labels, y_labels, figsize)
 
-
-def plot_box(df: pd.DataFrame, x_col: str, title: str) -> None:
-  plt.figure(figsize=(12, 7))
-  sns.boxplot(data=df, x=x_col)
-  plt.title(title, size=20)
-  plt.xticks(rotation=75, fontsize=14)
-  plt.show()
-
-
-def plot_box_multi(df: pd.DataFrame, x_col: str, y_col: str, title: str) -> None:
-  plt.figure(figsize=(12, 7))
-  sns.boxplot(data=df, x=x_col, y=y_col)
-  plt.title(title, size=20)
-  plt.xticks(rotation=75, fontsize=14)
-  plt.yticks(fontsize=14)
-  plt.show()
-
-
-def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str, style: str) -> None:
-  plt.figure(figsize=(12, 7))
-  sns.scatterplot(data=df, x=x_col, y=y_col, hue=hue, style=style)
-  plt.title(title, size=20)
-  plt.xticks(fontsize=14)
-  plt.yticks(fontsize=14)
-  plt.show()
+  def boxplot(self, df, y_value, title="", subtitles="", rows=1, cols=1, x_labels="", y_labels="", figsize=(12, 10)):
+    plot = lambda r, c, ax: sns.boxplot(y=df[y_value[(r * cols) + c]], ax=ax(r, c))
+    self.subplots(plot, rows, cols, title, subtitles, x_labels, y_labels, figsize)
+  
