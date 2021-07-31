bfd2aa494d8b2bd04368122bf3d897d04cf75c91
Elias <eandualem@gmail.com>
2021-07-31 10:47:15 +0300

    feat: add a function to load and save models

--- scripts/dvc_helper.py
+++ scripts/dvc_helper.py
@@ -1,9 +1,13 @@
 import os
 import sys
+import pickle
+import dvc.api
 import pandas as pd
+from config import Config
+from log import get_logger
+from time import gmtime, strftime
 
 sys.path.append(os.path.abspath(os.path.join('../')))
-from log import get_logger
 
 my_logger = get_logger("DfHelper")
 
@@ -12,6 +16,16 @@ class DvcHelper():
 
   def __init__(self):
     pass
+
+  def get_dvc_file(self, file_path, version):
+    try:
+      data_url = dvc.api.get_url(path=str(file_path), repo=str(Config.REPO), rev=version)
+      df = pd.read_csv(data_url, sep=',')
+      my_logger.info("Load data from dvc")
+      return df
+
+    except Exception:
+      my_logger.exception("Error loading file from dvc")
 
   def save_csv(self, df, csv_path, index=False):
     try:
@@ -28,3 +42,22 @@ class DvcHelper():
       return df
     except FileNotFoundError:
       my_logger.exception("file not found")
+
+  def save_model(self, model, model_name):
+    try:
+      time = strftime("%Y-%m-%d-%H:%M:%S", gmtime())
+      name = Config.MODELS_PATH + model_name + time + ".pkl"
+      pickle.dump(model, open(str(name), "wb"))
+      my_logger.info("file saved as csv")
+
+    except Exception:
+      my_logger.exception("save failed")
+
+  def read_model(self, model_name):
+    try:
+      name = Config.MODELS_PATH + model_name + ".pkl"
+      model = pickle.load(open(name, "rb"))
+      my_logger.debug("model read as pkl")
+      return model
+    except FileNotFoundError:
+      my_logger.exception("model not found")
